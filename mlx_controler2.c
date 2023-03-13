9cc66726e65ddf90b7f790bd856281b832dbb062
Fatih Soymaz <fsoymaz@k1m27s07.42istanbul.com.tr>
2023-03-14 00:05:08 +0300

    hgfd

--- mlx_controler2.c
+++ mlx_controler2.c
@@ -6,7 +6,7 @@
 /*   By: fsoymaz <fsoymaz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 05:01:56 by fsoymaz           #+#    #+#             */
-/*   Updated: 2023/03/13 23:35:47 by fsoymaz          ###   ########.fr       */
+/*   Updated: 2023/03/14 00:04:27 by fsoymaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,16 +54,14 @@ int	ft_movement(int key, t_mlx *mlx)
 
 	if (key != 0 && key != 2 && key != 1 && key != 13 && key != 53)
 		return (1);
+	if (key == 53)
+		close_with();
 	move(t_map.map, &step, (key == 2) - (key == 0), (key == 1) - (key == 13));
 	if ((t_map.gate_col != t_map.p_col || t_map.gate_row != t_map.p_row))
 		t_map.map[t_map.gate_row][t_map.gate_col] = 'E';
 	if (t_map.gate_col == t_map.p_col && t_map.gate_row == t_map.p_row
 		&& t_map.current_coin == t_map.coin_count)
 		exit_checker(mlx);
-	if (key == 53)
-	{
-		close_with();
-	}
 	put_img(mlx, t_map.map);
 	write_s(mlx, step);
 	return (0);
